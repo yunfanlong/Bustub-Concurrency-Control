0fa85620e001254abbc6de2c3261ca88d8763298
Yunfan Long <81694496+yunfanlong@users.noreply.github.com>
2023-06-25 00:00:00 -0700

    Implement LockShared

--- src/concurrency/lock_manager.cpp
+++ src/concurrency/lock_manager.cpp
@@ -18,7 +18,66 @@
 namespace bustub {
 
 auto LockManager::LockShared(Transaction *txn, const RID &rid) -> bool {
+  if (txn->GetState() == TransactionState::ABORTED) {
+    return false;
+  }
+  if (txn->GetState() == TransactionState::SHRINKING) {
+    txn->SetState(TransactionState::ABORTED);
+    throw TransactionAbortException(txn->GetTransactionId(), AbortReason::LOCK_ON_SHRINKING);
+  }
+  if (txn->GetIsolationLevel() == IsolationLevel::READ_UNCOMMITTED) {
+    txn->SetState(TransactionState::ABORTED);
+    throw TransactionAbortException(txn->GetTransactionId(), AbortReason::LOCKSHARED_ON_READ_UNCOMMITTED);
+  }
+  txn->SetState(TransactionState::GROWING);
+  std::unique_lock<std::mutex> lk(latch_);
+  auto &lock_request_queue = lock_table_[rid];
+  auto &request_queue = lock_request_queue.request_queue_;
+  auto &cv = lock_request_queue.cv_;
+  auto txn_id = txn->GetTransactionId();
+  request_queue.emplace_back(txn_id, LockMode::SHARED);
+  txn->GetSharedLockSet()->emplace(rid);
+  txn_table_[txn_id] = txn;
 
+  bool can_grant = true;
+  bool is_kill = false;
+  for (auto &request : request_queue) {
+    if (request.lock_mode_ == LockMode::EXCLUSIVE) {
+      if (request.txn_id_ > txn_id) {
+        txn_table_[request.txn_id_]->SetState(TransactionState::ABORTED);
+        is_kill = true;
+      } else {
+        can_grant = false;
+      }
+    }
+    if (request.txn_id_ == txn_id) {
+      request.granted_ = can_grant;
+      break;
+    }
+  }
+  if (is_kill) {
+    cv.notify_all();
+  }
+
+  while (!can_grant) {
+    for (auto &request : request_queue) {
+      if (request.lock_mode_ == LockMode::EXCLUSIVE &&
+          txn_table_[request.txn_id_]->GetState() != TransactionState::ABORTED) {
+        break;
+      }
+      if (request.txn_id_ == txn_id) {
+        can_grant = true;
+        request.granted_ = true;
+      }
+    }
+    if (!can_grant) {
+      cv.wait(lk);
+    }
+    if (txn->GetState() == TransactionState::ABORTED) {
+      throw TransactionAbortException(txn->GetTransactionId(), AbortReason::DEADLOCK);
+    }
+  }
+  return true;
 }
 
 auto LockManager::LockExclusive(Transaction *txn, const RID &rid) -> bool {
